1392b3e936b2a16f87d61f5eab2a2bd15b42eef2
Naveen Polasa <naveenpolasa99@gmail.com>
2023-01-03 01:44:26 +0530

    update code files

--- index.js
+++ index.js
@@ -63,14 +63,28 @@ const projectsData = [
     live: "https://backroads-b.netlify.app/",
     git: "https://github.com/Naveen-Polasa/backroads-project",
   },
+  {
+    id: 8,
+    title: "Data Analytics",
+    category: "HTML-CSS",
+    src: "https://user-images.githubusercontent.com/91241718/210266303-f264c82f-93a4-4ecc-b708-727555427656.png",
+    live: "https://data-analytics-landing-page.netlify.app/",
+    git: "https://github.com/naveen-polasa/data-analytics-landing-page",
+  },
 ];
 
 const projectsContainer = document.querySelector(".projects-container");
+const btnsContainer = document.querySelector(".btns-container");
 
 window.addEventListener("DOMContentLoaded", () => {
-  const projects = projectsData
+  showProjects(projectsData);
+  showBtns();
+});
+
+function showProjects(data) {
+  const projects = data
     .map((project) => {
-      return `<article class="w-96 p-2 border rounded-3xl border-gray-700 text-center ">
+      return `<article class="w-96 h-[310px] p-2 border rounded-3xl border-gray-700 text-center ">
                 <p class="text-center my-5 hover:text-yellow-500 hover:scale-110 hover:-rotate-1  duration-1000">${project.title}</p>
                 <img class="m-auto w-80 rounded-lg hover:scale-105 duration-1000"
                     src=${project.src}
@@ -83,4 +97,41 @@ window.addEventListener("DOMContentLoaded", () => {
     })
     .join(" ");
   projectsContainer.innerHTML = projects;
-});
+}
+
+function showBtns() {
+  let btns = new Set([
+    "All",
+    ...projectsData.map((project) => {
+      return project.category;
+    }),
+  ]);
+  btns = [...btns];
+
+  let finalBtns = btns
+    .map((btn) => {
+      return `<button class="text-xl mb-8 mt-2 mx-4 py-1 px-3 hover:bg-green-500 rounded-lg hover:text-sky-700 duration-300 ease-linear filter-btn "  data-id=${btn}>${btn}</button>`;
+    })
+    .join(" ");
+
+  btnsContainer.innerHTML = finalBtns;
+
+  const filterBtns = btnsContainer.querySelectorAll(".filter-btn");
+
+  filterBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const category = e.currentTarget.dataset.id;
+      const projectsCategory = projectsData.filter((project) => {
+        if (project.category === category) {
+          return project;
+        }
+      });
+      console.log(category);
+      if (category === "All") {
+        showProjects(projectsData);
+      } else {
+        showProjects(projectsCategory);
+      }
+    });
+  });
+}
