116ff164ef5e2fe6f967e0680d1cd9a9610f10e8
Naveen Polasa <naveenpolasa99@gmail.com>
2023-01-04 13:35:16 +0530

    made responsive

--- index.js
+++
@@ -1,5 +0,0 @@
-const particles = document.querySelector("#particles-js");
-const body = document.querySelector("body");
-
-console.log(particles.style)
-console.log(document.documentElement.scrollHeight)
